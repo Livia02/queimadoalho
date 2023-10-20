fa5b3d1b0cbb7d9f2000afa9dba921f52cf907a0
Aluno <livia-cirilo110@aluno.senai.br>
2023-10-20 13:05:23 -0300

    Flask funcionando

--- usuarios/controller.py
+++ usuarios/controller.py
@@ -1,47 +1,105 @@
-from flask import Blueprint, request, session, url_for, redirect, flash, render_template
+import functools
 
-bp = Blueprint("Usuario", __name__ )
+from flask import Blueprint
+from flask import flash
+from flask import g
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import session
+from flask import url_for
 
-@bp.route("/login", methods=('POST', 'GET'))
+from database.connection import db
+from models import User
+
+bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+
+def login_required(view):
+    """View decorator that redirects anonymous users to the login page."""
+
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    """If a user id is stored in the session, load the user object from
+    the database into ``g.user``."""
+    user_id = session.get("user_id")
+
+    if user_id is not None:
+        g.user = db.session.get(User, user_id)
+    else:
+        g.user = None
+
+
+@bp.route("/register", methods=("GET", "POST"))
+def register():
+    """Register a new user.
+
+    Validates that the username is not already taken. Hashes the
+    password for security.
+    """
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        error = None
+
+        if not username:
+            error = "Username is required."
+        elif not password:
+            error = "Password is required."
+        elif db.session.execute(
+            db.select(db.select(User).filter_by(username=username).exists())
+        ).scalar():
+            error = f"User {username} is already registered."
+
+        if error is None:
+            # the name is available, create the user and go to the login page
+            db.session.add(User(username=username, password=password))
+            db.session.commit()
+            return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template("auth/register.html")
+
+
+@bp.route("/login", methods=("GET", "POST"))
 def login():
-    error = None
-    if request.method == 'POST':
-        email = request.form.get('email')
-        senha = request.form.get('senha')
+    """Log in a registered user by adding the user id to the session."""
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        error = None
+        select = db.select(User).filter_by(username=username)
+        user = db.session.execute(select).scalar()
 
-        from database.dados import alunos
-        for k,v in alunos.items():
-            if email == v.get('usuario') and senha == v.get('senha'):
-                session['user'] = v
-                return redirect(url_for('index'))
-            else:
-                error = "Usuario ou senha inválidos!"
+        if user is None:
+            error = "Incorrect username."
+        elif not user.check_password(password):
+            error = "Incorrect password."
 
-    return render_template("usuario/login.html", error=error)
+        if error is None:
+            # store the user id in a new session and return to the index
+            session.clear()
+            session["user_id"] = user.id
+            return redirect(url_for("index"))
 
-@bp.route("/perfil", methods=('POST', 'GET'))
-def perfil():
+        flash(error)
 
-    if 'user' not in session and False:
-        # se não tiver logado devolve para login
-        return redirect(url_for(login))
-    
-        # 1-Pegar dados do banco
-        #for k,v...in alunos...session['user']['email']
+    return render_template("auth/login.html")
 
-    if request.method == 'POST':
-        # lógica salvar
-        nome = request.form.get("nome")
-        email = request.form.get("email")
-
-        from database.dados import alunos
-        for k,v,x in alunos.items():
-            pass
-
-    return render_template("perfil.html") #, usuario=usuario
 
 @bp.route("/logout")
 def logout():
-    return redirect(url_for("login"))
-
-        
+    """Clear the current session, including the stored user id."""
+    session.clear()
+    return redirect(url_for("index"))
---
+++ usuarios/models.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash
+
+from database.connection import db
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    password_hash: Mapped[str]
+    
+    def set_password(self, value: str) -> None:
+        """Store the password as a hash for security."""
+        self.password_hash = generate_password_hash(value)
+
+    # allow password = "..." to set a password
+    password = property(fset=set_password)
+
+    def check_password(self, value: str) -> bool:
+        return check_password_hash(self.password_hash, value)
